5674e4add3babbab195ed2b897b86eff02b84c56
Charles Coutu-Nadeau <charles@coutunadeau.com>
2014-09-09 16:18:50 -0400

    Initial Commit

---
+++ HelloObjectiveC/HelloObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by Charles Coutu-Nadeau on 9/9/14.
+//  Copyright (c) 2014 ChurrosLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
